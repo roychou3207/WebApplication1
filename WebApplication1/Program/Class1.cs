575765f8ffa3cea7bca9a3eeb9eef9fa3f1bca18
roych <roych@DESKTOP-FASKFSJ>
2022-01-11 15:09:17 +0800

    加入專案檔案。

---
+++ WebApplication1/Program/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Program
+{
+    public class Class1
+    {
+        static int[] Question1(int[] nums, int target)
+        {
+            int[] array = new int[2];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var v = nums[i];
+
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        array = new int[] { i, j };
+                    }
+                }
+
+            }
+            return array;
+        }
+
+
+
+        static void Question4(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+        }
+
+
+        static bool Question5(int i)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine("y");
+                return true;
+            }
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("y");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("n");
+                return false;
+            }
+        }
+    }
+}
